4ecee22664229c981305d2db2d7ca28c9353a973
tejesh <tejeshreddy250@gmail.com>
2022-08-10 22:58:07 +0530

    Basic ui for all screens added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Stylesheet from './components/Stylesheet';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import BasicExample from './components/Navbar';
+import Login  from './components/Login';
+import Signup from './components/Signup';
+import Homepage from './components/Homepage';
+import Clickscounter from './components/Clickscounter';
+import Malicious from './components/Malicious';
+import Termsofservice from './components/Termsofservice';
+import Privacy from './components/Privacy';
+import Contact from './components/Contact';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // <Stylesheet></Stylesheet>
+  <div>
+  {/* <BasicExample></BasicExample> */}
+  {/* <Login></Login> */}
+  {/* <Signup></Signup> */}
+  <Homepage></Homepage>
+ <Clickscounter></Clickscounter>
+ <Malicious></Malicious>
+ <Termsofservice></Termsofservice>
+ <Privacy></Privacy>  
+ <Contact></Contact>
+  </div>
   );
 }
 
